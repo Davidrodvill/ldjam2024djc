02af5a5624706b87a7fc993da9a89b2e6802b0cc
ChrisBChicken05 <144267132+ChrisBChicken05@users.noreply.github.com>
2024-04-14 19:11:34 -0400

    WHY DOES IT WORK ON THE LEFT SIDE BUT NOT THE RIGHT??????!!!!!! AAAAAAAAAAA

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -11,6 +11,7 @@ public class PlayerMovement : MonoBehaviour
     public float jumpForce = 5f;       
     public Rigidbody2D rb;              
     private bool isGrounded = true;
+    public bool canMove = true;
 
     [Header("Death")]
     public GameObject deathScreen;
@@ -23,6 +24,7 @@ public class PlayerMovement : MonoBehaviour
 
     [Header("Animation")]
     public Animator anim;
+    public bool facingRight = true;
 
 
 
@@ -31,8 +33,9 @@ public class PlayerMovement : MonoBehaviour
         Cursor.visible = false; //MAKE SURE TO SET THIS TO ACTIVE WHEN INTERACTING WITH MENUS
         retryButton.SetActive(false);
         menuButton.SetActive(false);
-
+        canMove = true;
         anim = GetComponent<Animator>();
+        facingRight = true;
     }
 
     void Update()
@@ -46,8 +49,52 @@ public class PlayerMovement : MonoBehaviour
 
     void MovePlayer()
     {
-        float horizontalInput = Input.GetAxisRaw("Horizontal"); //right and left movement
-        rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y); // Set the horizontal velocity
+        if(canMove)
+        {
+
+
+            
+
+            if (Input.GetAxisRaw("Horizontal") > 0 ) //right
+            {
+                
+                //rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y); // Set the horizontal velocity
+                transform.position += new Vector3(moveSpeed * Time.deltaTime, 0);
+
+                if (facingRight) //if we are facing right, we flip the player's sprite
+                {
+                    
+                    facingRight = false;
+                    transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z);
+                }
+                anim.SetBool("isWalking", true);
+
+            }
+
+            if (Input.GetAxisRaw("Horizontal") < 0) //left
+            {
+                transform.position -= new Vector3(moveSpeed * Time.deltaTime, 0);
+
+                
+                if (!facingRight) //if we facing right, we flip the player's sprite
+                {
+                    
+                    facingRight = true;
+                    transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+
+                }
+                anim.SetBool("isWalking", true);
+            }
+            else
+            {
+                anim.SetBool("isWalking", false);
+            }
+
+        }
+        
+
+
+        
     }
 
     void Jump()
