8c1ac9b288e65e96be3910789f22d1069598c6eb
xfAthanasia <146591035+xfAthanasia@users.noreply.github.com>
2024-04-13 22:17:22 -0400

    i hate this game

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -15,7 +15,7 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //change to a and d movement with a jump
     }
 
     // Update is called once per frame
